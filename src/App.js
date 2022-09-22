8367fe16ec03bfb58a7e6c0105a1d7921e36df88
Khromov <i.khromov@3commas.io>
2022-09-22 16:04:17 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const ref = useRef();
+  useEffect(() => {
+    if (ref.current) {
+      window.mercuryoWidget.run({
+        widgetId: "a8c1dead-ed5f-4740-b9ce-c4ea7721c93b",
+        host: ref.current,
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={ref}></div>
     </div>
   );
 }
